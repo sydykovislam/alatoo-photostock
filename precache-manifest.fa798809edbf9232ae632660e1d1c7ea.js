b0abf6a3c8aa402c4d4ec32fae1870afba9df6a2
sydykovislam <sydykovislam03@gmail.com>
2022-01-06 20:40:08 +0600

    Updates

---
+++ precache-manifest.fa798809edbf9232ae632660e1d1c7ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc996ae99f482c8ee875089737f4c935",
+    "url": "/alatoo-photostock/index.html"
+  },
+  {
+    "revision": "d7dab8c4745eb43e2e8f",
+    "url": "/alatoo-photostock/static/css/main.68d698e3.chunk.css"
+  },
+  {
+    "revision": "b5118ed6166bc00a8f7f",
+    "url": "/alatoo-photostock/static/js/2.bbcceb06.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/alatoo-photostock/static/js/2.bbcceb06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7dab8c4745eb43e2e8f",
+    "url": "/alatoo-photostock/static/js/main.60536554.chunk.js"
+  },
+  {
+    "revision": "8d776780d60cf2f2a08b",
+    "url": "/alatoo-photostock/static/js/runtime-main.4678c82b.js"
+  },
+  {
+    "revision": "e5536f78071518366883943661cc0c4f",
+    "url": "/alatoo-photostock/static/media/logo.e5536f78.png"
+  }
+]);
